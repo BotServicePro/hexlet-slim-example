56169b04619b484dd4d88fba37df46da87d49e02
akarakin <bulik889@gmail.com>
2021-02-26 12:15:44 +0200

    edited

--- public/index.php
+++ public/index.php
@@ -6,14 +6,36 @@ namespace Index;
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория, в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
+$users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
+
+
 $app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!');
-    return $response;
-    // Благодаря пакету slim/http этот же код можно записать короче
-    // return $response->write('Welcome to Slim!');
+    return $response->write('Main Page');
 });
+
+$app->get('/users', function ($request, $response, $args) use ($users) {
+    $searchRequest = $request->getQueryParam('term');
+    if ($searchRequest != null) {
+        $result = [];
+        foreach ($users as $user) {
+            strpos($user, $searchRequest) !== false ? $result[] = $user : null;
+        }
+        $params = ['users' => $users, 'searchRequest' => $searchRequest, 'result' => $result];
+        return $this->get('renderer')->render($response, "users/index.phtml", $params);
+    } else {
+        $params = ['users' => $users, 'result' => null];
+        return $this->get('renderer')->render($response, "users/index.phtml", $params);
+    }
+});
+
 $app->run();
